7e100da0d69b97d024b0c0a806c510c4defb4acf
Simon Elbrink <simon.elbrink@lexicon.se>
2022-09-06 10:07:22 +0200

    Spring Boot + JPA Lecture - Init

---
+++ spring_boot-jpa-lecture/src/main/java/se/lexicon/spring_bootjpalecture/SpringBootJpaLectureApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_bootjpalecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaLectureApplication.class, args);
+	}
+
+}
---
+++ spring_boot-jpa-lecture/src/test/java/se/lexicon/spring_bootjpalecture/SpringBootJpaLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_bootjpalecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
