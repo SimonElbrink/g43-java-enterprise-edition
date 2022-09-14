5c2a1072b62aebbb6e9ce84878184026234a3a02
Simon Elbrink <simon.elbrink@lexicon.se>
2022-09-14 10:10:24 +0200

    JPA - Workshop - Library Part 1

---
+++ jpa-workshop-library/src/main/java/se/lexicon/jpaworkshoplibrary/JpaWorkshopLibraryApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshoplibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWorkshopLibraryApplication.class, args);
+    }
+
+}
---
+++ jpa-workshop-library/src/test/java/se/lexicon/jpaworkshoplibrary/JpaWorkshopLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshoplibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
