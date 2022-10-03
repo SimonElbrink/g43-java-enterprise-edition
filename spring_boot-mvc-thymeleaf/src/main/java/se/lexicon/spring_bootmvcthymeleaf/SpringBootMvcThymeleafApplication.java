6e26f10e1e0483dc0b031f0414272e45de86b798
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-03 12:19:09 +0200

    Thymeleaf - Lecture - init

---
+++ spring_boot-mvc-thymeleaf/src/main/java/se/lexicon/spring_bootmvcthymeleaf/SpringBootMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_bootmvcthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ spring_boot-mvc-thymeleaf/src/test/java/se/lexicon/spring_bootmvcthymeleaf/SpringBootMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_bootmvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
