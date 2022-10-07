f95aadb617c86614be2a7950f8bf9233a7ba8afe
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-07 10:01:41 +0200

    Thymeleaf - Lecture - View Specific category-view.html (Detail)

--- spring_boot-mvc-thymeleaf/src/main/java/se/lexicon/spring_bootmvcthymeleaf/controller/CategoryController.java
+++ spring_boot-mvc-thymeleaf/src/main/java/se/lexicon/spring_bootmvcthymeleaf/controller/CategoryController.java
@@ -4,6 +4,7 @@ package se.lexicon.spring_bootmvcthymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.lexicon.spring_bootmvcthymeleaf.model.dto.CategoryView;
 
@@ -16,14 +17,15 @@ import java.util.List;
 public class CategoryController {
 
 
+
     List<CategoryView> categoryViewList = new ArrayList<>();
 
     public CategoryController() {
-        categoryViewList.add(new CategoryView(1, "Clothes", LocalDate.now()));
-        categoryViewList.add(new CategoryView(2, "Scarf", LocalDate.now()));
-        categoryViewList.add(new CategoryView(3, "Electronics", LocalDate.now()));
-        categoryViewList.add(new CategoryView(4, "Fuel", LocalDate.now()));
-        categoryViewList.add(new CategoryView(5, "Food", LocalDate.now()));
+        categoryViewList.add(new CategoryView(1,"Clothes", LocalDate.now()));
+        categoryViewList.add(new CategoryView(2,"Scarf", LocalDate.now()));
+        categoryViewList.add(new CategoryView(3,"Electronics", LocalDate.now()));
+        categoryViewList.add(new CategoryView(4,"Fuel", LocalDate.now()));
+        categoryViewList.add(new CategoryView(5,"Food", LocalDate.now()));
     }
 
     //http://localhost:8080/category/list
@@ -33,6 +35,21 @@ public class CategoryController {
         model.addAttribute("categoryViews", categoryViewList);
         model.addAttribute("categoryListSize", categoryViewList.size());
         return "category/categories-view";
+    }
+
+
+    //http://localhost:8080/category/view/1
+    //http://localhost:8080/category/view/5
+    @GetMapping("/view/{id}")
+    public String findById(@PathVariable("id") int id, Model model){
+
+        System.out.println("ID: "+ id);
+
+        CategoryView categoryView = categoryViewList.stream().filter(category -> category.getId() == id ).findFirst().orElse(null);
+
+        model.addAttribute("categoryView", categoryView);
+
+        return "category/category-view";
     }
 
 }
