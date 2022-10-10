c5673d0ed439d4c75e573f346eb5699d4d7474d0
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-10 17:12:11 +0200

    Thymeleaf - EXTRA - Locale Changing
    ?/lang=value - In URL changes The Language.
    
    ex.
    http://localhost:8080/dashboard?lang=sv
    = Show in Swedish
    
    http://localhost:8080/..?lang=en
    = Show in English

--- spring_boot-mvc-thymeleaf/src/main/java/se/lexicon/spring_bootmvcthymeleaf/config/WebMvcConfig.java
+++ spring_boot-mvc-thymeleaf/src/main/java/se/lexicon/spring_bootmvcthymeleaf/config/WebMvcConfig.java
@@ -3,7 +3,9 @@ package se.lexicon.spring_bootmvcthymeleaf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -12,10 +14,22 @@ import java.util.Locale;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(new Locale("sv"));
+        sessionLocaleResolver.setDefaultLocale(new Locale("en"));
         return sessionLocaleResolver;
     }
 
+    // http://localhost:8080/...?lang=sv
+    // http://localhost:8080/...?lang=en
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor());
+    }
 }
