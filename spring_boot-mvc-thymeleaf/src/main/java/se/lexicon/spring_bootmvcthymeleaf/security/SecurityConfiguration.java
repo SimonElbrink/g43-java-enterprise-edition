822645e4cdc727aeadc7e35a6e51303f113a4e62
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-14 17:40:21 +0200

    Thymeleaf - Extra-Lecture - Spring Security. Using built in Login form + Sign-out
    - with inMemory deprecated password encoder.

---
+++ spring_boot-mvc-thymeleaf/src/main/java/se/lexicon/spring_bootmvcthymeleaf/security/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package se.lexicon.spring_bootmvcthymeleaf.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeRequests()
+                .antMatchers("/webjars/**", "/images/**", "/css/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.
+                formLogin();
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails guest = User.withDefaultPasswordEncoder()
+                .username("guest").password("guest").roles("GUEST").build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user").password("user").roles("GUEST", "USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin").password("admin").roles("GUEST", "USER", "ADMIN").build();
+
+
+
+        return new InMemoryUserDetailsManager(guest,user,admin);
+    }
+
+
+
+}
