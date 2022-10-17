11e39b00ccef0168e3240d3dceac77fcb99c8ec6
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-17 10:28:07 +0200

    RestFul Webservices - Lecture - init method

---
+++ src/main/java/se/lexicon/spring_bootrestful_api/SpringBootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.spring_bootrestful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestfulApiApplication.class, args);
+    }
+
+}
