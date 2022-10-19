a22c49712576575b59d1f7f54168d8bd53e3d950
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-19 11:05:13 +0200

    RestFul Webservices - Lecture - Crud For RoleController.

--- spring_boot-restful_api/src/main/java/se/lexicon/spring_bootrestful_api/controller/RoleController.java
+++ spring_boot-restful_api/src/main/java/se/lexicon/spring_bootrestful_api/controller/RoleController.java
@@ -3,10 +3,8 @@ package se.lexicon.spring_bootrestful_api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.spring_bootrestful_api.dto.RoleFormDto;
 import se.lexicon.spring_bootrestful_api.entity.Role;
 import se.lexicon.spring_bootrestful_api.repository.RoleRepository;
 
@@ -26,19 +24,21 @@ public class RoleController {
 
     //GET http://localhost:8080/api/v1/roles/
     @GetMapping("/api/v1/roles")
-    public ResponseEntity<List<Role>> findAll(){
+    public ResponseEntity<List<Role>> findAll() {
 
         System.out.println("###   Get Roles has been executed!   ###");
 
         List<Role> listOfRoles = repository.findAll();
 
+//        return ResponseEntity.status(HttpStatus.OK).body(listOfRoles);
+//        return ResponseEntity.status(200).body(listOfRoles);
         return ResponseEntity.ok(listOfRoles);
     }
 
     //GET http://localhost:8080/api/v1/roles/2
     //GET http://localhost:8080/api/v1/roles/99
     @GetMapping("api/v1/roles/{id}")
-    public ResponseEntity<Role> findByRoleId(@PathVariable("id") Integer id){
+    public ResponseEntity<Role> findByRoleId(@PathVariable("id") Integer id) {
         Optional<Role> foundById = repository.findById(id);
 
 //        if (foundById.isPresent()){
@@ -54,7 +54,7 @@ public class RoleController {
 
     //GET http://localhost:8080/api/v1/roles/search?name=ADMIN
     @GetMapping("api/v1/roles/search")
-    public ResponseEntity<Role> findByRoleName(@RequestParam("name") String name){
+    public ResponseEntity<Role> findByRoleName(@RequestParam("name") String name) {
         Optional<Role> foundByName = repository.findByName(name);
 
 //        if (foundByName.isPresent()){
@@ -64,6 +64,57 @@ public class RoleController {
 //        }
         return foundByName.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NO_CONTENT).build());
+    }
+
+    /*
+        PUT http://localhost:8080/api/v1/roles
+        Content-Type: application/json
+
+        {
+            "name": "super-admin"
+        }
+    */
+    @PostMapping("/api/v1/roles")
+    public ResponseEntity<Role> create(@RequestBody RoleFormDto roleForm) {
+        System.out.println("###### In Create method");
+
+        Role role = new Role(roleForm.getId(), roleForm.getName());
+
+        Role savedRole = repository.save(role);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedRole);
+    }
+
+    /*
+        PUT http://localhost:8080/api/v1/roles/4
+        Content-Type: application/json
+
+        {
+            "id": 4,
+            "name": "SUPER-ADMIN"
+        }
+     */
+    @PutMapping("api/v1/roles/{id}")
+    public ResponseEntity<Void> update(@PathVariable("id") Integer id, @RequestBody RoleFormDto roleForm) {
+        System.out.println("###### Delete Method ######");
+        System.out.println("id = " + id);
+        System.out.println("roleForm = " + roleForm);
+
+        if (id.equals(roleForm.getId())) {
+            Role role = new Role(roleForm.getId(), roleForm.getName());
+            repository.save(role);
+
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(418).build();
+        }
+    }
+
+    //DELETE http://localhost:8080/api/v1/roles/4
+    @DeleteMapping("api/v1/roles/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+        System.out.println("id = " + id);
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
---
+++ spring_boot-restful_api/src/main/java/se/lexicon/spring_bootrestful_api/dto/RoleFormDto.java
@@ -0,0 +1,15 @@
+package se.lexicon.spring_bootrestful_api.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RoleFormDto {
+
+    private Integer id;
+    private String name;
+}
