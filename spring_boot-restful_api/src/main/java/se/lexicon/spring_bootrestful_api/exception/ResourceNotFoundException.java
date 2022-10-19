b15001d91d70a92a6faf83d59d1fa5a4b83198d3
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-19 11:56:22 +0200

    RestFul Webservices - Lecture - IMPORTANT Fix Exception added.

---
+++ spring_boot-restful_api/src/main/java/se/lexicon/spring_bootrestful_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package se.lexicon.spring_bootrestful_api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+    }
+}
