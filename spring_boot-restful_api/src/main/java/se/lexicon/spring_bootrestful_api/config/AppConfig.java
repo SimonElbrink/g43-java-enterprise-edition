f51a163b7e2f04159d800f258e2e62186e26901c
Simon Elbrink <simon.elbrink@lexicon.se>
2022-10-21 09:32:54 +0200

    RestFul Webservices - lecture - Added OpenAPI / SwaggerUI

--- spring_boot-restful_api/src/main/java/se/lexicon/spring_bootrestful_api/config/AppConfig.java
+++ spring_boot-restful_api/src/main/java/se/lexicon/spring_bootrestful_api/config/AppConfig.java
@@ -1,10 +1,13 @@
 package se.lexicon.spring_bootrestful_api.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "G43 Restful API Lecture", version = "0.1", description = "INFORMATION ABOUT PROJECT"))
 public class AppConfig {
 
     @Bean
